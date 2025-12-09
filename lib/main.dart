ae2ee8a52b5f1f40f294c3065a0370b72ff21cdf
Lê Quang Long <quanglong.3824@gmail.com>
2025-12-09 17:41:26 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quản lý Nhân viên',
+      title: 'Quản lý Nhân viên - Hoàng Quân',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
